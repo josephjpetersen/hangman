e82f2bdffbcdd00ebf7ae7f40f447b8d10f08db3
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-19 16:39:44 -0400

    Write play_game and get_guess methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
+require 'colorize'
+
 class Game
+
   def initialize(txt_file)
     @secret_word = get_secret_word(txt_file)
     @clues = ''
@@ -12,17 +15,39 @@ class Game
     txt_file[index]
   end
   
-  def play_game
-    p @clues
-    puts "Guess a letter:"
-    until @attempts.zero? || @clues.include?('_') == false
-      guess = gets.chomp
-      correct_guess? = @secret_word.include?(guess)
-
-      if correct_guess?
-        @secret_word.each_with_index do |letter, index|
-
+  def get_guess
+    loop do
+      guess = gets.chomp.downcase
+      if guess.length == 1 && ('a'..'z').include?(guess)
+        if @guesses.map { |letter| letter.uncolorize }.include?(guess)
+          puts "You've already guessed that letter. Try again.".colorize(:light_red)
+        else
+          return guess
         end
+      else
+        puts "Invalid input. Try again.".colorize(:light_red)
+      end
+    end
+  end
+
+  def play_game
+    until @attempts.zero? || @clues.include?('_') == false
+      puts "\n#{@clues}"
+      puts "Guesses so far: #{@guesses.join(' ')}"
+      puts "Attempts remaining: #{@attempts}"
+      puts "Enter a letter:"
+      guess = get_guess
+
+      if @secret_word.include?(guess)
+        @guesses << guess.colorize(:light_green)
+        @secret_word.chars.each_with_index do |letter, index|
+          if letter == guess
+            @clues[index] = letter
+          end
+        end
+      else
+        @guesses << guess.colorize(:light_red)
+        @attempts -= 1
       end
     end
   end
--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,7 @@
+require_relative 'game.rb'
+
 txt_file = File.read('google-10000-english-no-swears.txt').split
   .select{ |word| word.length > 4}
   .select{ |word| word.length < 13}
+
+Game.new(txt_file).play_game
